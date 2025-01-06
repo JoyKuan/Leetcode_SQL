7c8ac8d2f4618e823a0f8b143fdc05f61955a3ee
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-06 15:25:07 -0800

    Time: 1018 ms (16.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT person_name
+FROM (
+    SELECT person_name,
+    SUM(weight) OVER(ORDER BY turn) AS Total_Weight
+    FROM Queue
+)
+WHERE Total_Weight <= 1000
+ORDER BY Total_Weight DESC
+LIMIT 1
+
