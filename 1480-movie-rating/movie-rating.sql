df032a43271e6e66bdad1ece6775f6e11373a9b8
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-06 00:10:59 -0800

    Time: 811 ms (5.04%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -3,13 +3,11 @@
 (SELECT name AS results
 FROM Users
 RIGHT JOIN (
-    SELECT user_id,
-    DENSE_RANK() OVER(ORDER BY COUNT(DISTINCT movie_id) DESC) AS rnk
+    SELECT user_id, COUNT(movie_id) AS greatest_num
     FROM MovieRating
     GROUP BY user_id
 ) u ON Users.user_id = u.user_id
-WHERE rnk=1
-ORDER BY name 
+ORDER BY greatest_num DESC, name 
 LIMIT 1)
 
 UNION ALL
@@ -17,13 +15,11 @@ UNION ALL
 (SELECT title AS results
 FROM Movies
 RIGHT JOIN (
-    SELECT movie_id,
-        DENSE_RANK() OVER(ORDER by AVG(rating) DESC) AS rnk
+    SELECT movie_id, AVG(rating) AS avg
     FROM MovieRating
     WHERE TO_CHAR(created_at, 'YYYY-MM')='2020-02'
     GROUP BY movie_id
     ) avg ON Movies.movie_id = avg.movie_id
-WHERE rnk=1
-ORDER BY title
+ORDER BY avg DESC, title 
 LIMIT 1)
 
