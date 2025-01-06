1a0bf7e509e8ea60402fee616e4d615894a40e0b
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-06 15:26:02 -0800

    Time: 857 ms (27.95%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,12 @@
 -- Write your PostgreSQL query statement below
-SELECT person_name
-FROM (
+WITH cum_sum AS(
     SELECT person_name,
     SUM(weight) OVER(ORDER BY turn) AS Total_Weight
     FROM Queue
 )
+
+SELECT person_name
+FROM cum_sum
 WHERE Total_Weight <= 1000
 ORDER BY Total_Weight DESC
 LIMIT 1
