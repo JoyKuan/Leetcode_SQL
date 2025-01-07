f5dcc489ed1de4695a6192fecf0a2f93f517e1f6
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-07 15:33:08 -0800

    Time: 5757 ms (5.15%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,29 +1,25 @@
 -- Write your PostgreSQL query statement below
-WITH account_cate AS(
-    SELECT account_id, 
-        CASE
-            WHEN income < 20000 THEN 'Low Salary'
-            WHEN income >= 20000 AND income <= 50000 THEN 'Average Salary'
-            WHEN income > 50000 THEN 'High Salary'
-            ELSE NULL
-        END AS category
+WITH Low_Salary AS(
+    SELECT 'Low Salary' AS category, 
+        SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS accounts_count
     FROM Accounts
 ),
 
--- initial category table
-init_cate AS(
-    SELECT 'Low Salary' AS category
-    UNION ALL
-    SELECT 'Average Salary'
-    UNION ALL
-    SELECT 'High Salary'
+Avg_Salary AS(
+    SELECT 'Average Salary' AS category, 
+        SUM(CASE WHEN income >= 20000 AND income <= 50000 THEN 1 ELSE 0 END) AS accounts_count
+    FROM Accounts
+),
+
+High_Salary AS(
+    SELECT 'High Salary' AS category, 
+        SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
+    FROM Accounts
 )
 
-SELECT ic.category, COALESCE(accounts_count, 0) AS accounts_count
-FROM init_cate ic
-LEFT JOIN (
-    SELECT category, COUNT(*) AS accounts_count
-    FROM account_cate
-    GROUP BY category) cate
-ON ic.category = cate.category
 
+SELECT * FROM Low_Salary
+UNION ALL 
+SELECT * FROM Avg_Salary
+UNION ALL 
+SELECT * FROM High_Salary
