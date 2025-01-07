72ba4443da0a5004faded8ffc9934200d7821aaf
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-07 15:40:23 -0800

    Time: 3476 ms (87.07%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,25 +1,6 @@
 -- Write your PostgreSQL query statement below
-WITH Low_Salary AS(
-    SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count
-    FROM Accounts
-    WHERE income < 20000
-),
-
-Avg_Salary AS(
-    SELECT 'Average Salary' AS category, COUNT(*) AS accounts_count
-    FROM Accounts
-    WHERE income >= 20000 AND income <= 50000
-),
-
-High_Salary AS(
-    SELECT 'High Salary' AS category, COUNT(*) AS accounts_count
-    FROM Accounts
-    WHERE income > 50000
-)
-
-
-SELECT * FROM Low_Salary
-UNION ALL 
-SELECT * FROM Avg_Salary
-UNION ALL 
-SELECT * FROM High_Salary
+(SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count FROM Accounts WHERE income < 20000)
+UNION
+(SELECT 'Average Salary', COUNT(*) AS accounts_count FROM Accounts WHERE income >= 20000 AND income <= 50000)
+UNION
+(SELECT 'High Salary', COUNT(*) AS accounts_count FROM Accounts WHERE income > 50000)
