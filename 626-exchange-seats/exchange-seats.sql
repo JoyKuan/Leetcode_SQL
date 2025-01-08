4e57b166dd7c50f4bc57c5d59de6cae7788efd84
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-07 16:25:54 -0800

    Time: 280 ms (44.17%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,24 +1,40 @@
 -- Write your PostgreSQL query statement below
-WITH even_id AS(
-    SELECT id, LAG(student) OVER(ORDER BY id) AS student FROM Seat
-),
-odd_id AS(
-    SELECT id, LEAD(student) OVER(ORDER BY id) AS student FROM Seat
-),
-swap_combine AS(
-    SELECT * FROM even_id WHERE id%2=0
-    UNION
-    SELECT * FROM odd_id WHERE id%2!=0
-)
+-- WITH even_id AS(
+--     SELECT id, LAG(student) OVER(ORDER BY id) AS student FROM Seat
+-- ),
+-- odd_id AS(
+--     SELECT id, LEAD(student) OVER(ORDER BY id) AS student FROM Seat
+-- ),
+-- swap_combine AS(
+--     SELECT * FROM even_id WHERE id%2=0
+--     UNION
+--     SELECT * FROM odd_id WHERE id%2!=0
+-- )
 
 
 
-SELECT Seat.id,
-    CASE 
-        WHEN sc.student IS NULL THEN Seat.student ELSE sc.student
-    END
+-- SELECT Seat.id,
+--     CASE 
+--         WHEN sc.student IS NULL THEN Seat.student ELSE sc.student
+--     END
+-- FROM Seat
+-- LEFT JOIN swap_combine sc ON Seat.id = sc.id
+
+
+SELECT id,
+    CASE
+        WHEN id%2 = 0 THEN LAG(student) OVER(ORDER BY id)
+        WHEN id%2 = 1 AND LEAD(student) OVER(ORDER BY id) IS NULL THEN student ELSE LEAD(student) OVER(ORDER BY id)
+    END AS student
 FROM Seat
-LEFT JOIN swap_combine sc ON Seat.id = sc.id
+
+
+
+
+
+
+
+
 
 
 
