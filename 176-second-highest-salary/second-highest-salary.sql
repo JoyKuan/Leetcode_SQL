289e2d1ba4f400756f2b43bf8155d6572ed3b12e
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-10 00:04:12 -0800

    Time: 380 ms (26.41%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,10 +1,10 @@
 -- Write your PostgreSQL query statement below
--- WITH CTE AS(
-    
--- )
+WITH CTE AS(
+    SELECT id, salary,
+    DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+    FROM Employee
+)
 
 SELECT MAX(salary) AS SecondHighestSalary
-FROM (SELECT id, salary,
-    DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
-    FROM Employee)
+FROM CTE
 WHERE rnk=2
