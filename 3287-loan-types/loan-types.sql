c6fea92e161a857581290c979a18f585f04f0ccf
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-19 12:54:22 -0800

    Time: 471 ms (34.61%) | Memory: 0B (100.00%) - LeetSync

--- 3287-loan-types/loan-types.sql
+++ 3287-loan-types/loan-types.sql
@@ -3,5 +3,5 @@ SELECT user_id
 FROM Loans
 WHERE loan_type IN ('Refinance', 'Mortgage')
 GROUP BY user_id
-HAVING COUNT(distinct loan_type) = 2
+HAVING COUNT(DISTINCT loan_type) = 2
 ORDER BY user_id
