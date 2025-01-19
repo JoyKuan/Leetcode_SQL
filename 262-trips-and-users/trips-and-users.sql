6e99436fb922f266c09535c45d9674530237f4fb
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-19 14:45:01 -0800

    Time: 406 ms (23.86%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,13 +1,9 @@
 -- Write your PostgreSQL query statement below
-WITH not_ban AS(
-    SELECT users_id
-    FROM Users
-    WHERE banned='No'
-)
-
 SELECT request_at AS Day,
-    ROUND(SUM(CASE WHEN status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1.0 ELSE 0 END) / COUNT(*), 2) AS "Cancellation Rate"
+    ROUND(AVG(CASE WHEN status = 'completed' THEN 0 ELSE 1 END), 2) AS "Cancellation Rate"
 FROM Trips
-WHERE (request_at between '2013-10-01' and '2013-10-03') AND (client_id IN (SELECT * FROM not_ban)) AND (driver_id IN (SELECT * FROM not_ban)) 
+WHERE (request_at between '2013-10-01' and '2013-10-03') AND 
+(client_id NOT IN (SELECT users_id FROM Users WHERE banned='Yes' AND role='client')) AND 
+(driver_id NOT IN (SELECT users_id FROM Users WHERE banned='Yes' AND role='driver')) 
 GROUP BY request_at
 
