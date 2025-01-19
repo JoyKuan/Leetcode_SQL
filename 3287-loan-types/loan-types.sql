f74681aa85acdd70410d4bc38a5007be7b34064a
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-19 12:53:24 -0800

    Time: 291 ms (71.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3287-loan-types/loan-types.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT user_id
+FROM Loans
+WHERE loan_type IN ('Refinance', 'Mortgage')
+GROUP BY user_id
+HAVING COUNT(distinct loan_type) = 2
+ORDER BY user_id
