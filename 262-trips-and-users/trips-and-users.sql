2b6869dda256295bfbf2ef2b94817aa13e77a532
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-19 14:34:12 -0800

    Time: 322 ms (44.50%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -6,7 +6,7 @@ WITH not_ban AS(
 )
 
 SELECT request_at AS Day,
-    ROUND(AVG(CASE WHEN status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1.0 ELSE 0 END), 2) AS "Cancellation Rate"
+    ROUND(AVG(CASE WHEN status = 'completed' THEN 0 ELSE 1 END), 2) AS "Cancellation Rate"
 FROM Trips
 WHERE (request_at between '2013-10-01' and '2013-10-03') AND (client_id IN (SELECT * FROM not_ban)) AND (driver_id IN (SELECT * FROM not_ban)) 
 GROUP BY request_at
