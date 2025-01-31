eb06675e055cd19f24fbf093b2a2b03fda68c4d5
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-31 15:31:37 -0800

    Time: 1774 ms (48.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+-- In test case, 'S8' is not number 1
+
+SELECT buyer_id
+FROM Sales s
+LEFT JOIN Product p ON s.product_id = p.product_id
+GROUP BY buyer_id
+HAVING SUM(CASE WHEN product_name='S8' THEN 1 ELSE 0 END) > 0 AND SUM(CASE WHEN product_name='iPhone' THEN 1 ELSE 0 END) = 0
+
+
