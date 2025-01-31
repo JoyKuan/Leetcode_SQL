0b04c9a990ee0b2ce35c1a40c79c56bf83f7048a
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-01-31 15:32:13 -0800

    Time: 1043 ms (87.16%) | Memory: 0B (100.00%) - LeetSync

--- 1173-sales-analysis-ii/sales-analysis-ii.sql
+++ 1173-sales-analysis-ii/sales-analysis-ii.sql
@@ -3,7 +3,7 @@
 
 SELECT buyer_id
 FROM Sales s
-LEFT JOIN Product p ON s.product_id = p.product_id
+JOIN Product p ON s.product_id = p.product_id
 GROUP BY buyer_id
 HAVING SUM(CASE WHEN product_name='S8' THEN 1 ELSE 0 END) > 0 AND SUM(CASE WHEN product_name='iPhone' THEN 1 ELSE 0 END) = 0
 
