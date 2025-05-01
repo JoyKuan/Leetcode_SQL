eda28cba183989d823d2250d81c22b2590868708
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-01 00:00:32 -0700

    Time: 679 ms (80.49%) | Memory: 0B (100.00%) - LeetSync

--- 1377-number-of-comments-per-post/number-of-comments-per-post.sql
+++ 1377-number-of-comments-per-post/number-of-comments-per-post.sql
@@ -13,6 +13,6 @@ post_cte AS(
 )
 
 SELECT sub_id AS post_id, 
-    CASE WHEN cnt IS NULL THEN 0 ELSE cnt END AS number_of_comments
+    COALESCE(cnt, 0) AS number_of_comments
 FROM post_cte t1
 LEFT JOIN parent_cte t2 ON t1.sub_id = t2.parent_id
