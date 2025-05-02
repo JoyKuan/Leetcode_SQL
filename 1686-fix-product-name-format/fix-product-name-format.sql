99c01769d9918102ca17457a57fdc9d52ec7a211
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-02 00:16:53 -0700

    Time: 369 ms (50.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1686-fix-product-name-format/fix-product-name-format.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    LOWER(TRIM(both ' ' FROM product_name)) AS product_name,
+    DATE_FORMAT(sale_date, '%Y-%m') AS sale_date,
+    COUNT(*) AS total
+FROM Sales
+GROUP BY LOWER(TRIM(both ' ' FROM product_name)), DATE_FORMAT(sale_date, '%Y-%m') 
+ORDER BY product_name ASC, sale_date ASC
