f6d5a35c1deb3af454ff7f1b25908f8ce79a398a
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-04 16:36:58 -0700

    Time: 331 ms (67.27%) | Memory: 0B (100.00%) - LeetSync

--- 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
+++ 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
@@ -1,7 +1,21 @@
 -- Write your PostgreSQL query statement below
-SELECT DISTINCT A.user_id AS user_id
-FROM Confirmations A
-INNER JOIN Confirmations B
-ON A.user_id = B.user_id 
-AND A.time_stamp < B.time_stamp
-AND B.time_stamp - A.time_stamp <= INTERVAL '24 hours'
+-- SELECT DISTINCT A.user_id AS user_id
+-- FROM Confirmations A
+-- INNER JOIN Confirmations B
+-- ON A.user_id = B.user_id 
+-- AND A.time_stamp < B.time_stamp
+-- AND B.time_stamp - A.time_stamp <= INTERVAL '24 hours'
+
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT user_id
+FROM (
+    SELECT
+        user_id,
+        time_stamp,
+        LEAD(time_stamp) OVER(PARTITION BY user_id ORDER BY time_stamp) AS next_time
+    FROM Confirmations
+) 
+WHERE next_time IS NOT NULL AND next_time - time_stamp <= INTERVAL '24 hours'
+
+
+
