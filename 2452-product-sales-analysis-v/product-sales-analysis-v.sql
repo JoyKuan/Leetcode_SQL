3c9290227831666fcbe14de3e3cdf8bcf0738e94
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-04 23:35:32 -0700

    Time: 367 ms (34.21%) | Memory: 0B (100.00%) - LeetSync

--- 2452-product-sales-analysis-v/product-sales-analysis-v.sql
+++ 2452-product-sales-analysis-v/product-sales-analysis-v.sql
@@ -3,6 +3,6 @@ SELECT
     user_id,
     SUM(quantity*price) AS spending
 FROM Sales
-LEFT JOIN Product ON Sales.product_id=Product.product_id
+INNER JOIN Product ON Sales.product_id=Product.product_id
 GROUP BY user_id
 ORDER BY spending DESC, user_id ASC
