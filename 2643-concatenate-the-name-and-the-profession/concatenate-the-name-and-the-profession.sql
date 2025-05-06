74506d66f1d9368a8be78626b7b3a8427e604710
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-05 23:48:05 -0700

    Time: 275 ms (42.50%) | Memory: 0B (100.00%) - LeetSync

--- 2643-concatenate-the-name-and-the-profession/concatenate-the-name-and-the-profession.sql
+++ 2643-concatenate-the-name-and-the-profession/concatenate-the-name-and-the-profession.sql
@@ -1,6 +1,13 @@
 -- Write your PostgreSQL query statement below
+-- SELECT
+--     person_id,
+--     CONCAT(name, '(', LEFT(profession, 1),')' ) AS name
+-- FROM Person
+-- ORDER BY person_id DESC
+
+
 SELECT
     person_id,
-    CONCAT(name, '(', LEFT(profession, 1),')' ) AS name
+    name || '(' || LEFT(profession, 1) || ')' AS name
 FROM Person
 ORDER BY person_id DESC
