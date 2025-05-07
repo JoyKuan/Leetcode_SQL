4cf1f8c8a3b2456f5a08e73f8b4839e2a6af09ea
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-07 00:07:21 -0700

    Time: 307 ms (11.76%) | Memory: 0B (100.00%) - LeetSync

--- 2829-find-latest-salaries/find-latest-salaries.sql
+++ 2829-find-latest-salaries/find-latest-salaries.sql
@@ -1,10 +1,22 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    emp_id,
-    firstname,
-    lastname,
-    MAX(CAST(salary AS INT)) AS salary,
-    department_id
-FROM Salary
-GROUP BY emp_id, firstname, lastname, department_id
+-- SELECT
+--     emp_id,
+--     firstname,
+--     lastname,
+--     MAX(CAST(salary AS INT)) AS salary,
+--     department_id
+-- FROM Salary
+-- GROUP BY emp_id, firstname, lastname, department_id
+-- ORDER BY emp_id
+
+
+
+-- Method 2
+SELECT emp_id, firstname, lastname, salary, department_id
+FROM (
+    SELECT *, RANK() OVER(PARTITION BY emp_id ORDER BY CAST(salary AS INT) DESC) rnk
+    FROM Salary
+)
+WHERE rnk=1
 ORDER BY emp_id
+
