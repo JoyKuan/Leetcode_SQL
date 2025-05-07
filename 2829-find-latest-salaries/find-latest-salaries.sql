7f5ba62b6f7a2ac7c27c4a0142b164f2b9a061cb
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-07 00:11:17 -0700

    Time: 231 ms (38.23%) | Memory: 0B (100.00%) - LeetSync

--- 2829-find-latest-salaries/find-latest-salaries.sql
+++ 2829-find-latest-salaries/find-latest-salaries.sql
@@ -11,12 +11,19 @@
 
 
 
--- Method 2
-SELECT emp_id, firstname, lastname, salary, department_id
-FROM (
-    SELECT *, RANK() OVER(PARTITION BY emp_id ORDER BY CAST(salary AS INT) DESC) rnk
-    FROM Salary
-)
-WHERE rnk=1
-ORDER BY emp_id
+-- Method 2: 單表掃描 + 窗口函數排序
+-- SELECT emp_id, firstname, lastname, salary, department_id
+-- FROM (
+--     SELECT *, RANK() OVER(PARTITION BY emp_id ORDER BY CAST(salary AS INT) DESC) rnk
+--     FROM Salary
+-- )
+-- WHERE rnk=1
+-- ORDER BY emp_id
+
+-- Method 3: DISTINCT ON (ONLY PostgreSQL)
+SELECT DISTINCT ON (emp_id) *
+FROM Salary
+ORDER BY emp_id, salary DESC
+
+
 
