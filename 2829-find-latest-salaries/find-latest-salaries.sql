285199dd1fbe48c23b604dfb027e4810986232a2
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-07 00:31:27 -0700

    Time: 236 ms (38.23%) | Memory: 0B (100.00%) - LeetSync

--- 2829-find-latest-salaries/find-latest-salaries.sql
+++ 2829-find-latest-salaries/find-latest-salaries.sql
@@ -1,26 +1,26 @@
 -- Write your PostgreSQL query statement below
--- SELECT
---     emp_id,
---     firstname,
---     lastname,
---     MAX(CAST(salary AS INT)) AS salary,
---     department_id
--- FROM Salary
--- GROUP BY emp_id, firstname, lastname, department_id
--- ORDER BY emp_id
+SELECT
+    emp_id,
+    firstname,
+    lastname,
+    MAX(CAST(salary AS INT)) AS salary,
+    department_id
+FROM Salary
+GROUP BY emp_id, firstname, lastname, department_id
+ORDER BY emp_id
 
 
 
 -- Method 2: 單表掃描 + 窗口函數排序
-SELECT emp_id, firstname, lastname, salary, department_id
-FROM (
-    SELECT *, ROW_NUMBER() OVER(PARTITION BY emp_id ORDER BY CAST(salary AS INT) DESC) rnk
-    FROM Salary
-)
-WHERE rnk=1
-ORDER BY emp_id
+-- SELECT emp_id, firstname, lastname, salary, department_id
+-- FROM (
+--     SELECT *, ROW_NUMBER() OVER(PARTITION BY emp_id ORDER BY CAST(salary AS INT) DESC) rnk
+--     FROM Salary
+-- )
+-- WHERE rnk=1
+-- ORDER BY emp_id
 
--- Method 3: DISTINCT ON (ONLY PostgreSQL)
+-- -- Method 3: DISTINCT ON (ONLY PostgreSQL)
 -- SELECT DISTINCT ON (emp_id) *
 -- FROM Salary
 -- ORDER BY emp_id, salary DESC
