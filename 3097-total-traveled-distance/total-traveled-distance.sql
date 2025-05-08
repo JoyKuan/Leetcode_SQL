c280597f8312ce433ef6f70359718053851e7c9b
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-08 00:08:39 -0700

    Time: 236 ms (36.67%) | Memory: 0B (100.00%) - LeetSync

--- 3097-total-traveled-distance/total-traveled-distance.sql
+++ 3097-total-traveled-distance/total-traveled-distance.sql
@@ -2,10 +2,10 @@
 SELECT 
     u.user_id, 
     name, 
-    COALESCE(total_distance, 0) AS "traveled distance"
+    COALESCE(dist, 0) AS "traveled distance"
 FROM Users u
 LEFT JOIN (
-    SELECT user_id, SUM(distance) AS total_distance
+    SELECT user_id, SUM(distance) AS dist
     FROM Rides
     GROUP BY user_id
 ) r ON u.user_id = r.user_id
