a7cf8b226a86a2ff95fbd48a85ac5c00b27b56e9
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-08 15:04:56 -0700

    Time: 171 ms (93.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3256-find-candidates-for-data-scientist-position/find-candidates-for-data-scientist-position.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT candidate_id
+FROM Candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(*) >= 3
+ORDER BY candidate_id 
