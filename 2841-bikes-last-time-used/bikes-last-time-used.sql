04a28c88987edcbbc8b457445bae6774848d8657
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-07 23:56:35 -0700

    Time: 231 ms (31.43%) | Memory: 0B (100.00%) - LeetSync

--- 2841-bikes-last-time-used/bikes-last-time-used.sql
+++ 2841-bikes-last-time-used/bikes-last-time-used.sql
@@ -1,12 +1,16 @@
 -- Write your PostgreSQL query statement below
-SELECT bike_number, end_time
-FROM (
-    SELECT 
-    *,
-    ROW_NUMBER() OVER(PARTITION BY bike_number ORDER BY end_time DESC) rnk
-    FROM Bikes
-)
-WHERE rnk=1
+-- SELECT bike_number, end_time
+-- FROM (
+--     SELECT 
+--     *,
+--     ROW_NUMBER() OVER(PARTITION BY bike_number ORDER BY end_time DESC) rnk
+--     FROM Bikes
+-- )
+-- WHERE rnk=1
+-- ORDER BY end_time DESC
+
+
+SELECT bike_number, MAX(end_time) AS end_time
+FROM Bikes
+GROUP BY bike_number
 ORDER BY end_time DESC
-
-
