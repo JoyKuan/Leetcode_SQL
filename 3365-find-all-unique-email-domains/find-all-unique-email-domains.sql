f3f6ecce968c903ac2cec54de19774134c78ec98
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-09 16:44:19 -0700

    Time: 212 ms (52.78%) | Memory: 0B (100.00%) - LeetSync

--- 3365-find-all-unique-email-domains/find-all-unique-email-domains.sql
+++ 3365-find-all-unique-email-domains/find-all-unique-email-domains.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
-SELECT SUBSTRING(email FROM '@(\w+\.com)') AS email_domain, COUNT(email) AS count
+SELECT SUBSTRING(email FROM '@(.*)$') AS email_domain, COUNT(email) AS count
 FROM Emails
-WHERE email ~ '@(\w+\.com)'
+WHERE email ~ '^.*?@.*\.com$'
 GROUP BY email_domain
 ORDER BY email_domain
