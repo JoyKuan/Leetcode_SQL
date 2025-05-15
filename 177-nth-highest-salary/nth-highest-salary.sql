a23a6be9277db89fe5042f03fafe5307a0500ab9
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-14 23:44:50 -0700

    Time: 227 ms (51.18%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS
 BEGIN
   RETURN QUERY (
 
-    SELECT e.Salary 
-    FROM (SELECT e.Salary,DENSE_RANK() OVER(ORDER BY e.Salary DESC) rnk FROM Employee e) e
-    WHERE e.rnk = N
+    SELECT a.Salary 
+    FROM (SELECT e.Salary,DENSE_RANK() OVER(ORDER BY e.Salary DESC) rnk FROM Employee e) a
+    WHERE a.rnk = N
     LIMIT 1
     
   );
