7afe56ed96d9c3c0cb2c88efbc4b03983cddc317
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-14 23:40:54 -0700

    Time: 294 ms (18.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+
+    WITH emp_rank AS (
+        SELECT 
+            DISTINCT e.Salary,
+            DENSE_RANK() OVER(ORDER BY e.Salary DESC) rnk
+        FROM Employee e
+    )
+
+    SELECT e.Salary 
+    FROM emp_rank e
+    WHERE e.rnk = N
+    
+  );
+END;
+$$ LANGUAGE plpgsql;
