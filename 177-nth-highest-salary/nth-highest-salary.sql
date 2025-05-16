46dcfd671a5730e51f6144513998ae054242351a
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-15 19:15:33 -0700

    Time: 224 ms (54.83%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,12 +1,23 @@
 CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
 BEGIN
+IF N < 1 THEN
+    RETURN QUERY(SELECT NULL::INT AS salary);
+ELSE
   RETURN QUERY (
-
-    SELECT a.Salary 
-    FROM (SELECT e.Salary,DENSE_RANK() OVER(ORDER BY e.Salary DESC) rnk FROM Employee e) a
-    WHERE a.rnk = N
-    LIMIT 1
-    
+    SELECT DISTINCT Employee.salary
+    FROM Employee
+      ORDER BY salary DESC
+      LIMIT 1
+      OFFSET N-1
   );
+END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+
+--    SELECT a.Salary 
+--     FROM (SELECT e.Salary,DENSE_RANK() OVER(ORDER BY e.Salary DESC) rnk FROM Employee e) a
+--     WHERE a.rnk = N
+--     LIMIT 1
