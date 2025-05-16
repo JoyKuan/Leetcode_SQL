3af2e6144588a41ca0298f9ebf1d70cff16c419d
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-15 21:06:59 -0700

    Time: 203 ms (74.22%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,9 +1,19 @@
 -- Write your PostgreSQL query statement below
-SELECT question_id AS survey_log
-FROM SurveyLog
-GROUP BY question_id
-ORDER BY COUNT(answer_id)*1.0 / SUM(CASE WHEN action='show' THEN 1 ELSE 0 END) DESC, question_id
-LIMIT 1
+with cte as(
+    select question_id,
+    sum(case when action = 'answer' then 1 else 0 end)*1.0/count(distinct (id, question_id, timestamp)) as ans_rate
+    from surveylog
+    group by 1)
+
+select min(question_id) as survey_log from cte 
+WHERE ans_rate = (SELECT MAX(ans_rate) FROM cte)
+having count(*) > 0;
+
+-- SELECT question_id AS survey_log
+-- FROM SurveyLog
+-- GROUP BY question_id
+-- ORDER BY COUNT(answer_id)*1.00 / SUM(CASE WHEN action='show' THEN 1 ELSE 0 END) DESC, question_id
+-- LIMIT 1
 
 
 
