e99c98c202930f431164b6b4bc4859bab47ed34d
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-15 19:47:27 -0700

    Time: 190 ms (66.67%) | Memory: 0B (100.00%) - LeetSync

--- 574-winning-candidate/winning-candidate.sql
+++ 574-winning-candidate/winning-candidate.sql
@@ -1,11 +1,24 @@
 -- Write your PostgreSQL query statement below
+-- SELECT name
+-- FROM Candidate c
+-- JOIN (
+--     SELECT candidateId
+--     FROM Vote
+--     GROUP BY candidateId
+--     ORDER BY COUNT(*) DESC
+--     LIMIT 1
+-- ) v ON c.id=v.candidateId
+
+
 SELECT name
 FROM Candidate c
-JOIN (
-    SELECT candidateId, COUNT(*) AS votes
+WHERE id IN (
+    SELECT candidateId
     FROM Vote
     GROUP BY candidateId
     ORDER BY COUNT(*) DESC
     LIMIT 1
-) v ON c.id=v.candidateId
+)
+    
+
 
