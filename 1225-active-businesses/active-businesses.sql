36432c713714a86b11a53aff90b46e0afa270b4b
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-17 15:58:07 -0700

    Time: 917 ms (44.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1225-active-businesses/active-businesses.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+WITH avg_activity AS(
+    SELECT event_type, AVG(occurrences) AS avg
+    FROM Events
+    GROUP BY event_type
+)
+
+SELECT business_id
+FROM Events e
+JOIN avg_activity a ON e.event_type = a.event_type
+GROUP BY business_id
+HAVING SUM(CASE WHEN occurrences > avg THEN 1 ELSE 0 END) > 1
