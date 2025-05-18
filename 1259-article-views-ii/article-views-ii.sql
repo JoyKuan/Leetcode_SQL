3e6a687ae18acec078d031287016029218adb66e
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-17 21:16:27 -0700

    Time: 359 ms (53.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1259-article-views-ii/article-views-ii.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT viewer_id AS id
+FROM (
+    SELECT view_date, viewer_id
+    FROM Views
+    GROUP BY view_date, viewer_id
+    HAVING COUNT(DISTINCT article_id) > 1
+) 
+ORDER BY id
+
+
