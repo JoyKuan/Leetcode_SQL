e0bf35e83e226c1d3f54deb705df7b79708fe3d0
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-20 14:44:04 -0700

    Time: 155 ms (99.60%) | Memory: 0B (100.00%) - LeetSync

--- 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,9 +1,20 @@
 -- Write your PostgreSQL query statement below
-with recursive cte as (select employee_id from employees where employee_id=1
-                        UNION
-                        select e.employee_id
-                        from employees e join cte c on e.manager_id = c.employee_id
-                        )
-select * from cte
+WITH RECURSIVE manager_cte AS(
+    SELECT employee_id
+    FROM Employees
+    WHERE employee_id = 1
+
+    UNION
+
+    SELECT e.employee_id
+    FROM Employees e
+    JOIN manager_cte m ON e.manager_id = m.employee_id
+)
+
+select * from manager_cte
 where employee_id!=1
+
+-- e 是整張員工表（所有人）
+-- c 是已經知道的「主管群」（從 CTE 累積而來）
+-- 所以你是從「所有人（e）」中，找出那些 他的上司在已知主管清單（c）裡的人。
 
