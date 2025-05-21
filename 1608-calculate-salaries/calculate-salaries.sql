75f2890e2fee045f8199a13bbda051d62c997760
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-20 23:36:40 -0700

    Time: 388 ms (77.61%) | Memory: 0B (100.00%) - LeetSync

--- 1608-calculate-salaries/calculate-salaries.sql
+++ 1608-calculate-salaries/calculate-salaries.sql
@@ -3,7 +3,7 @@ WITH company_tax_rate AS(
     SELECT
         company_id, 
         CASE
-            WHEN MAX(salary) < 1000 THEN 0.00
+            WHEN MAX(salary) < 1000 THEN 0
             WHEN MAX(salary) between 1000 and 10000 THEN 0.24
             ELSE 0.49
         END AS tax_rate
@@ -15,6 +15,6 @@ SELECT
     s.company_id,
     employee_id,
     employee_name,
-    ROUND(salary - tax_rate * salary) AS salary
+    ROUND(salary * (1 - tax_rate)) AS salary
 FROM Salaries s
 INNER JOIN company_tax_rate ctr ON s.company_id = ctr.company_id
