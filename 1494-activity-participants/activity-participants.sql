70284e22e778011d03baa8bf3d113fd479aef592
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-20 17:56:07 -0700

    Time: 189 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 1494-activity-participants/activity-participants.sql
+++ 1494-activity-participants/activity-participants.sql
@@ -1,10 +1,14 @@
 -- Write your PostgreSQL query statement below
 WITH rank_activity AS(
-    SELECT activity, COUNT(*) AS cnt
+    SELECT 
+        activity, 
+        COUNT(*) AS cnt, 
+        MAX(COUNT(*)) OVER () AS cnt_max,
+        MIN(COUNT(*)) OVER () AS cnt_min
     FROM Friends
     GROUP BY activity
 )
 
 SELECT activity
 FROM rank_activity
-WHERE cnt != (SELECT max(cnt) FROM rank_activity) AND cnt != (SELECT min(cnt) FROM rank_activity)
+WHERE cnt NOT IN (cnt_min, cnt_max)
