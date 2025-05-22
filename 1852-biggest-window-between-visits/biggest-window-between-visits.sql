5bea58a69965e4074a482d6f2c5fafff8c7eb831
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-21 23:30:12 -0700

    Time: 258 ms (81.45%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -1,12 +1,12 @@
 -- Write your PostgreSQL query statement below
-WITH detail_UserVisits AS(
+WITH UserVisits_cte AS(
     SELECT
         user_id,
         visit_date,
-        COALESCE(LEAD(visit_date, 1) OVER(PARTITION BY user_id ORDER BY visit_date), '2021-1-1'::DATE) AS next_day
+        LEAD(visit_date, 1, '2021-1-1') OVER(PARTITION BY user_id ORDER BY visit_date) AS next_day
     FROM UserVisits
 )
 
 SELECT user_id, MAX(next_day - visit_date) AS biggest_window
-FROM detail_UserVisits
+FROM UserVisits_cte
 GROUP BY user_id
