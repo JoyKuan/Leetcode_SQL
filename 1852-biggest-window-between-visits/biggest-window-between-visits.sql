421cff7a968ba5961078747f141d831c06c221cc
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-21 23:33:15 -0700

    Time: 341 ms (11.31%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -3,7 +3,7 @@ WITH UserVisits_cte AS(
     SELECT
         user_id,
         visit_date,
-        LEAD(visit_date, 1, '2021-1-1') OVER(PARTITION BY user_id ORDER BY visit_date) AS next_day
+        LEAD(visit_date, 1, '2021-1-1'::date) OVER(PARTITION BY user_id ORDER BY visit_date) AS next_day
     FROM UserVisits
 )
 
