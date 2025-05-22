823a705e27c362350a58fc41f4ae504eab280cbf
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-05-22 16:15:19 -0700

    Time: 225 ms (81.63%) | Memory: 0B (100.00%) - LeetSync

--- 2252-the-airport-with-the-most-traffic/the-airport-with-the-most-traffic.sql
+++ 2252-the-airport-with-the-most-traffic/the-airport-with-the-most-traffic.sql
@@ -1,17 +1,13 @@
 -- Write your PostgreSQL query statement below
 WITH airport_cte AS(
-    SELECT departure_airport AS airport_id, SUM(flights_count) AS flight_cnt
-    FROM Flights
-    GROUP BY departure_airport
+    SELECT departure_airport AS airport_id, flights_count FROM Flights
     UNION ALL
-    SELECT arrival_airport AS airport_id, SUM(flights_count) AS flight_cnt
-    FROM Flights
-    GROUP BY arrival_airport
+    SELECT arrival_airport AS airport_id, flights_count FROM Flights
 ),
 airport_cte2 AS(
     SELECT airport_id, 
-        SUM(flight_cnt) AS flight_total,
-        RANK() OVER(ORDER BY SUM(flight_cnt) DESC) AS rnk
+        SUM(flights_count) AS flight_total,
+        RANK() OVER(ORDER BY SUM(flights_count) DESC) AS rnk
     FROM airport_cte
     GROUP BY airport_id
 )
