b34eb0ad6b97acb217aa7deb825fc7fb40574da3
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-06-28 15:28:30 -0700

    Time: 393 ms (97.62%) | Memory: 0B (100.00%) - LeetSync

--- 3283-find-third-transaction/find-third-transaction.sql
+++ 3283-find-third-transaction/find-third-transaction.sql
@@ -1,12 +1,35 @@
 -- Write your PostgreSQL query statement below
-WITH rnk_cte AS(
-    SELECT *, RANK() OVER(PARTITION BY user_id ORDER BY transaction_date) AS rnk
-    FROM Transactions
-    ORDER BY user_id, transaction_date
-)
+with t as(select 
+    user_id, 
+    spend,
+    transaction_date, 
+    lead(transaction_date) over w third_transaction_date,
+    row_number() over w rn,
+    lag(spend) over w  prev_spend,
+    lead(spend) over w next_spend
+from transactions
+window w as (partition by user_id order by transaction_date))
+select user_id, next_spend third_transaction_spend, third_transaction_date 
+from t
+where rn = 2
+and next_spend > spend
+and next_spend > prev_spend
+order by user_id asc
 
-SELECT user_id, spend AS third_transaction_spend, transaction_date AS third_transaction_date
-FROM rnk_cte c
-WHERE rnk = 3 AND 
-spend > (SELECT spend FROM rnk_cte WHERE user_id = c.user_id and rnk = 2) AND 
-spend > (SELECT spend FROM rnk_cte WHERE user_id = c.user_id and rnk = 1)
+
+
+
+
+
+-- Not good
+-- WITH rnk_cte AS(
+--     SELECT *, RANK() OVER(PARTITION BY user_id ORDER BY transaction_date) AS rnk
+--     FROM Transactions
+--     ORDER BY user_id, transaction_date
+-- )
+
+-- SELECT user_id, spend AS third_transaction_spend, transaction_date AS third_transaction_date
+-- FROM rnk_cte c
+-- WHERE rnk = 3 AND 
+-- spend > (SELECT spend FROM rnk_cte WHERE user_id = c.user_id and rnk = 2) AND 
+-- spend > (SELECT spend FROM rnk_cte WHERE user_id = c.user_id and rnk = 1)
