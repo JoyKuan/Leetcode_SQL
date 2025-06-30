aaa571e9f8178127c4d0f97a8e582c487f92223b
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-06-29 21:35:39 -0700

    Time: 205 ms (92.48%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+WITH sub_cte AS(
+    SELECT 
+        user_id,
+        ROUND(AVG(CASE WHEN activity_type='free_trial' THEN activity_duration END), 2) AS trial_avg_duration,
+        ROUND(AVG(CASE WHEN activity_type='paid' THEN activity_duration END), 2) AS paid_avg_duration
+    FROM UserActivity
+    GROUP BY user_id
+    ORDER BY user_id
+)
+
+SELECT *
+FROM sub_cte
+WHERE trial_avg_duration IS NOT NULL AND paid_avg_duration IS NOT NULL
+
+
