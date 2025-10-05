a360d353b6171fbc0be6d80a5a81b06060c9a787
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-05 15:47:53 -0700

    Time: 190 ms (79.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+WITH single_num AS (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*)=1
+)
+
+SELECT MAX(num) AS num
+FROM single_num
