3fe7ed110d4349c2e31ba94b21700630ba10429b
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-04 21:40:27 -0700

    Time: 274 ms (99.78%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,9 @@
 -- Write your PostgreSQL query statement below
-select e2.name AS name
-from Employee e1
-inner join Employee e2 on e1.managerId = e2.id
+select e2.name as name
+from Employee e1, Employee e2
+where e1.managerId = e2.id
 group by e2.id, e2.name
-HAVING COUNT(*) >=5
+having count(*) >= 5
+
 
 
