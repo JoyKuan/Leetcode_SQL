8d6c9d1c0a42fa2cddca6bba878e4d03f49ff6a8
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-05 13:38:44 -0700

    Time: 400 ms (93.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+ select 
+    TO_CHAR(trans_date, 'YYYY-MM') AS month,
+    country,
+    count(*) AS trans_count,
+    SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
+ from Transactions
+ group by country, TO_CHAR(trans_date, 'YYYY-MM')
