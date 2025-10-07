c416cc92bce7ac0613899b22d0dae430a0bc8597
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-07 10:52:44 -0700

    Time: 173 ms (96.73%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -2,7 +2,7 @@
 select id,
     case
         when id%2=0 then LAG(student) OVER(ORDER BY id) 
-        when id%2!=0 and LEAD(student) OVER(ORDER BY id) IS NOT NULL then LEAD(student) OVER(ORDER BY id) else student
+        when id%2=1 and LEAD(student) OVER(ORDER BY id) IS NOT NULL then LEAD(student) OVER(ORDER BY id) else student
     end AS student
 from Seat
 order by id
