029ebb2655c1dfc8e02f3f2529f759e777f6eb33
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-07 15:06:38 -0700

    Time: 235 ms (97.84%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,11 +1,9 @@
 -- Write your PostgreSQL query statement below
-WITH high_earner AS(
-    SELECT name AS Employee, salary AS Salary, departmentId,
-    DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rnk
-    FROM Employee
-)
- 
-SELECT d.name AS Department, Employee, Salary
-FROM Department d
-RIGHT JOIN high_earner h ON h.departmentId = d.id
-WHERE rnk <=3
+select d.name AS Department, e.name AS Employee, salary AS Salary
+from (
+    select name, salary, departmentId,
+    dense_rank() over(partition by departmentId order by salary desc) AS rnk
+    from Employee
+) e
+left join Department d on e.departmentId = d.id
+where rnk <=3
