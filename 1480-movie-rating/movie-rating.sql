761d311e325d8d45994ddad93fcfa3e6afb79289
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-07 11:27:23 -0700

    Time: 245 ms (75.26%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,25 +1,16 @@
 -- Write your PostgreSQL query statement below
-
-(SELECT name AS results
-FROM Users
-RIGHT JOIN (
-    SELECT user_id, COUNT(movie_id) AS greatest_num
-    FROM MovieRating
-    GROUP BY user_id
-) u ON Users.user_id = u.user_id
-ORDER BY greatest_num DESC, name 
-LIMIT 1)
-
+(select name AS results
+from MovieRating mr
+left join Users u on mr.user_id = u.user_id
+group by name
+order by count(*) desc, name
+limit 1)
 UNION ALL
-
-(SELECT title AS results
-FROM Movies
-RIGHT JOIN (
-    SELECT movie_id, AVG(rating) AS avg
-    FROM MovieRating
-    WHERE TO_CHAR(created_at, 'YYYY-MM')='2020-02'
-    GROUP BY movie_id
-    ) avg ON Movies.movie_id = avg.movie_id
-ORDER BY avg DESC, title 
-LIMIT 1)
+(select title AS results
+from MovieRating mr
+left join Movies v on mr.movie_id = v.movie_id
+where TO_CHAR(created_at, 'YYYY-MM') = '2020-02'
+group by title
+order by AVG(rating) desc, title
+limit 1)
 
