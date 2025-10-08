db0ee9d2a61738d5ff70b6f5ab70ad95188a4fe1
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-07 23:14:43 -0700

    Time: 3 ms (90.74%) | Memory: 21.2 MB (62.34%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,7 +1,6 @@
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import Counter
-
         count = Counter(nums)   
         return [k for k, v in count.most_common(k)]
 
