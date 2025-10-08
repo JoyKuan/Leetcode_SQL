ab6fcaf16c41e4cfa567c6fb20e47e351293d528
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-07 22:44:06 -0700

    Time: 12 ms (13.32%) | Memory: 19.9 MB (13.80%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,13 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        stack = []
-        for num in nums:
-            if num not in stack:
-                stack.append(num)
-            else:
-                stack.remove(num)
-        return stack.pop()
+        from collections import defaultdict
+        dic = defaultdict(int)
 
-
-
+        for n in nums:
+            dic[n] += 1
         
+        for n in dic:
+            if dic[n] == 1:
+                return n
+
+
+
+
+        #  stack = []
+        # for num in nums:
+        #     if num not in stack:
+        #         stack.append(num)
+        #     else:
+        #         stack.remove(num)
+        # return stack.pop()       
