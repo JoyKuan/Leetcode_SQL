21c2102abed89da0d6f171047b785c1d71cddac1
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-09 15:34:44 -0700

    Time: 215 ms (60.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
@@ -0,0 +1,17 @@
+-- Write your PostgreSQL query statement below
+with cte AS(
+    select distinct student_id
+    from (
+        select exam_id, student_id, score,
+            first_value(score) over(partition by exam_id order by score desc) AS max_score,
+            first_value(score) over(partition by exam_id order by score) AS min_score
+        from Exam
+    )
+    where score IN (min_score, max_score)
+)
+select student_id, student_name
+from Student
+where student_id not in (select student_id from cte) AND student_id in (select distinct student_id from Exam)
+
+
+
