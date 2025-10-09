511269f94a34589780d8aa6bdc08df00cebd4de7
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-09 13:01:21 -0700

    Time: 206 ms (90.95%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,23 +1,12 @@
 CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
 BEGIN
-IF N < 1 THEN
-    RETURN QUERY(SELECT NULL::INT AS salary);
-ELSE
   RETURN QUERY (
-    SELECT DISTINCT Employee.salary
-    FROM Employee
-      ORDER BY salary DESC
-      LIMIT 1
-      OFFSET N-1
+    -- Write your PostgreSQL query statement below.
+    select a.salary
+    from (select e.salary, dense_rank() over (order by e.salary desc) r from Employee e) a
+    where a.r = n
+    limit 1
+      
   );
-END IF;
 END;
 $$ LANGUAGE plpgsql;
-
-
-
-
---    SELECT a.Salary 
---     FROM (SELECT e.Salary,DENSE_RANK() OVER(ORDER BY e.Salary DESC) rnk FROM Employee e) a
---     WHERE a.rnk = N
---     LIMIT 1
