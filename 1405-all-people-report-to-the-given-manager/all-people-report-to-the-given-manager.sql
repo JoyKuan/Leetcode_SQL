9000fb125d00e5ac29137d68732def37bb083292
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-09 14:40:32 -0700

    Time: 172 ms (45.31%) | Memory: 0B (100.00%) - LeetSync

--- 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,20 +1,19 @@
 -- Write your PostgreSQL query statement below
-WITH RECURSIVE manager_cte AS(
-    SELECT employee_id
-    FROM Employees
-    WHERE employee_id = 1
+select e.employee_id AS employee_id
+from Employees e, Employees e1, Employees e2
+where e.manager_id = e1.employee_id AND e1.manager_id = e2.employee_id AND e.employee_id != 1 AND e2.manager_id = 1
 
-    UNION
 
-    SELECT e.employee_id
-    FROM Employees e
-    JOIN manager_cte m ON e.manager_id = m.employee_id
-)
 
-select * from manager_cte
-where employee_id!=1
-
--- e 是整張員工表（所有人）
--- c 是已經知道的「主管群」（從 CTE 累積而來）
--- 所以你是從「所有人（e）」中，找出那些 他的上司在已知主管清單（c）裡的人。
-
+-- +-------------+---------------+------------+
+-- | employee_id | employee_name | manager_id |employee_id | employee_name | manager_id | employee_id | employee_name | manager_id |
+-- +-------------+---------------+------------+
+-- | 1           | Boss          | 1          |1           | Boss          | 1          |1           | Boss          | 1          |
+-- | 3           | Alice         | 3          |3           | Alice         | 3          |3           | Alice         | 3          |
+-- | 4           | Daniel        | 2          |2           | Bob           | 1          |1           | Boss          | 1          |
+-- | 7           | Luis          | 4          |4           | Daniel        | 2          |2           | Bob           | 1          |
+-- | 8           | Jhon          | 3          |3           | Alice         | 3          |3           | Alice         | 3          |
+-- | 9           | Angela        | 8          |8           | Jhon          | 3          |3           | Alice         | 3          |
+-- | 2           | Bob           | 1          |1           | Boss          | 1          |1           | Boss          | 1          |
+-- | 77          | Robert        | 1          |1           | Boss          | 1          |1           | Boss          | 1          |
+-- +-------------+---------------+------------+
