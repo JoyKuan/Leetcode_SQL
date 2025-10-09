ab8e3d3844dd546e5474bd6169756d2392f6d954
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-08 23:00:16 -0700

    Time: 334 ms (68.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select stock_name,
+    sum(case when operation='Buy' then (-1)*price else price end) AS capital_gain_loss
+from Stocks
+group by stock_name
