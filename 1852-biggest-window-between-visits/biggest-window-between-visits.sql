5439516443fe076e77f8f438f7d9e3e7674e3a88
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-09 13:55:21 -0700

    Time: 249 ms (95.33%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -1,12 +1,30 @@
 -- Write your PostgreSQL query statement below
-WITH UserVisits_cte AS(
-    SELECT
-        user_id,
-        LEAD(visit_date, 1, '2021-1-1'::date) OVER(PARTITION BY user_id ORDER BY visit_date)-visit_date AS wd
-    FROM UserVisits
+select 
+    user_id,
+    max(next_date - visit_date) AS biggest_window
+from (
+    select user_id, visit_date,
+        LEAD(visit_date, 1, '2021-1-1'::date) over(partition by user_id order by visit_date) AS next_date
+    from UserVisits
 )
+group by user_id
 
-SELECT user_id, MAX(wd) AS biggest_window
-FROM UserVisits_cte
-GROUP BY user_id
-ORDER BY user_id
+
+
+
+
+
+
+
+
+
+-- +---------+------------+
+-- | user_id | visit_date | LEAD(visit_date)
+-- +---------+------------+
+-- | 1       | 2020-10-20 |2020-11-28
+-- | 1       | 2020-11-28 |2020-12-3 
+-- | 1       | 2020-12-3  | null  '2021-1-1'
+-- | 2       | 2020-10-5  |2020-12-9
+-- | 2       | 2020-12-9  |null  '2021-1-1'
+-- | 3       | 2020-11-11 |
+-- +---------+------------+
