a720eecab315cfe5f38eadbac5825ad91d55d564
JoyKuan <54604816+JoyKuan@users.noreply.github.com>
2025-10-09 17:15:33 -0700

    Time: 283 ms (73.33%) | Memory: 0B (100.00%) - LeetSync

--- 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
+++ 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
@@ -1,15 +1,21 @@
 -- Write your PostgreSQL query statement below
-with cte as(
-    select
-        task_id,
-        generate_series(1, subtasks_count, 1) AS subtask_id
-    from Tasks
-)
+-- with cte as(
+--     select
+--         task_id,
+--         generate_series(1, subtasks_count, 1) AS subtask_id
+--     from Tasks
+-- )
 
-select c.task_id, c.subtask_id
-from cte c
-left outer join Executed e on c.task_id = e.task_id AND c.subtask_id = e.subtask_id
-where e.task_id is null AND e.subtask_id is null
+-- select c.task_id, c.subtask_id
+-- from cte c
+-- left outer join Executed e on c.task_id = e.task_id AND c.subtask_id = e.subtask_id
+-- where e.task_id is null AND e.subtask_id is null
+
+
+select task_id, generate_series(1, subtasks_count, 1) as subtask_id
+from Tasks
+except
+select * from Executed
 
 
 
